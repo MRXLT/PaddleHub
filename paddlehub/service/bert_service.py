177be58ba9d8e327d7839a4acf64d0c79efd7d88
MRXLT <xlt2024@gmail.com>
2019-11-28 17:33:26 +0800

    use ujson

--- paddlehub/service/bert_service.py
+++ paddlehub/service/bert_service.py
@@ -3,7 +3,7 @@ import sys
 import time
 import numpy as np
 import paddlehub as hub
-import json
+import ujson
 import random
 from paddlehub.common.logger import logger
 
@@ -50,7 +50,6 @@ class BertService():
         con = httplib.HTTPConnection(ip, port)
         self.con_list.append(con)
         '''
-
     def connect_all_server(self, server_list):
         for server_str in server_list:
             self.server_list.append(server_str)
@@ -59,12 +58,11 @@ class BertService():
             port = int(port)
             self.con_list.append(httplib.HTTPConnection(ip, port))
             '''
-
     def data_convert(self, text):
         if self.reader_flag == False:
             module = hub.Module(name=self.model_name)
-            inputs, outputs, program = module.context(
-                trainable=True, max_seq_len=128)
+            inputs, outputs, program = module.context(trainable=True,
+                                                      max_seq_len=128)
             input_ids = inputs["input_ids"]
             position_ids = inputs["position_ids"]
             segment_ids = inputs["segment_ids"]
@@ -77,19 +75,20 @@ class BertService():
                 do_lower_case=self.do_lower_case)
             self.reader_flag = True
 
-        return self.reader.data_generator(
-            batch_size=self.batch_size, phase='predict', data=text)
+        return self.reader.data_generator(batch_size=self.batch_size,
+                                          phase='predict',
+                                          data=text)
 
     def infer(self, request_msg):
         if self.load_balance == 'round_robin':
             try:
-                cur_con = httplib.HTTPConnection(self.server_list[
-                    self.con_index])
+                cur_con = httplib.HTTPConnection(
+                    self.server_list[self.con_index])
                 cur_con.request('POST', "/BertService/inference", request_msg,
                                 {"Content-Type": "application/json"})
                 response = cur_con.getresponse()
                 response_msg = response.read()
-                response_msg = json.loads(response_msg)
+                response_msg = ujson.loads(response_msg)
                 self.con_index += 1
                 self.con_index = self.con_index % len(self.server_list)
                 return response_msg
@@ -109,13 +108,13 @@ class BertService():
                 random.seed()
                 self.con_index = random.randint(0, len(self.server_list) - 1)
                 logger.info(self.con_index)
-                cur_con = httplib.HTTPConnection(self.server_list[
-                    self.con_index])
+                cur_con = httplib.HTTPConnection(
+                    self.server_list[self.con_index])
                 cur_con.request('POST', "/BertService/inference", request_msg,
                                 {"Content-Type": "application/json"})
                 response = cur_con.getresponse()
                 response_msg = response.read()
-                response_msg = json.loads(response_msg)
+                response_msg = ujson.loads(response_msg)
 
                 return response_msg
             except BaseException as err:
@@ -134,13 +133,13 @@ class BertService():
 
             try:
                 self.con_index = int(self.process_id) % len(self.server_list)
-                cur_con = httplib.HTTPConnection(self.server_list[
-                    self.con_index])
+                cur_con = httplib.HTTPConnection(
+                    self.server_list[self.con_index])
                 cur_con.request('POST', "/BertService/inference", request_msg,
                                 {"Content-Type": "application/json"})
                 response = cur_con.getresponse()
                 response_msg = response.read()
-                response_msg = json.loads(response_msg)
+                response_msg = ujson.loads(response_msg)
 
                 return response_msg
             except BaseException as err:
@@ -173,14 +172,22 @@ class BertService():
             mask_list = batch[0][3].reshape(-1).tolist()
             for si in range(self.batch_size):
                 instance_dict = {}
-                instance_dict["token_ids"] = token_list[si * self.max_seq_len:(
-                    si + 1) * self.max_seq_len]
-                instance_dict["sentence_type_ids"] = sent_list[
-                    si * self.max_seq_len:(si + 1) * self.max_seq_len]
-                instance_dict["position_ids"] = pos_list[si * self.max_seq_len:(
-                    si + 1) * self.max_seq_len]
-                instance_dict["input_masks"] = mask_list[si * self.max_seq_len:(
-                    si + 1) * self.max_seq_len]
+                instance_dict["token_ids"] = token_list[si *
+                                                        self.max_seq_len:(si +
+                                                                          1) *
+                                                        self.max_seq_len]
+                instance_dict["sentence_type_ids"] = sent_list[si *
+                                                               self.max_seq_len:
+                                                               (si + 1) *
+                                                               self.max_seq_len]
+                instance_dict["position_ids"] = pos_list[si *
+                                                         self.max_seq_len:(si +
+                                                                           1) *
+                                                         self.max_seq_len]
+                instance_dict["input_masks"] = mask_list[si *
+                                                         self.max_seq_len:(si +
+                                                                           1) *
+                                                         self.max_seq_len]
                 request.append(instance_dict)
 
             copy_time = time.time() - copy_start
@@ -189,7 +196,7 @@ class BertService():
             request["max_seq_len"] = self.max_seq_len
             request["emb_size"] = self.embedding_size
             request["feed_var_names"] = self.feed_var_names
-            request_msg = json.dumps(request)
+            request_msg = ujson.dumps(request)
             if self.show_ids:
                 logger.info(request_msg)
             request_start = time.time()
@@ -230,13 +237,14 @@ class BertService():
 
 def test():
 
-    bc = BertService(
-        model_name='bert_chinese_L-12_H-768_A-12',
-        emb_size=768,
-        show_ids=True,
-        do_lower_case=True)
+    bc = BertService(model_name='bert_chinese_L-12_H-768_A-12',
+                     emb_size=768,
+                     show_ids=True,
+                     do_lower_case=True)
     bc.connect('127.0.0.1:8010')
-    result = bc.encode([["远上寒山石径斜"], ])
+    result = bc.encode([
+        ["远上寒山石径斜"],
+    ])
     #print(result[0])
     bc.close()
 
